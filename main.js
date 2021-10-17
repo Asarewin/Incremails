8f058ce47e49a5a7385e9ff0125190f8b1973d7a
Asarewin <46689120+Asarewin@users.noreply.github.com>
2021-10-17 20:09:26 +0200

    savegame implementation (doesnt work yet)

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
 var gameData = {
-
+// initialize basic game numbers
     mail: 0,
     totalMail: 0,
     mailPerClick : 1,
-	mailPerClickCost : 10
+	mailPerClickCost : 10,
 }
 
+window.onload = loadsave()
+
+function loadsave() {
+  var savegame = JSON.parse(localStorage.getItem("IncremailsSave"))
+    if (savegame !== null) {
+      gameData = savegame
+    }
+}
+
+//this function adds the number of mail per clicks each time button is pressed
 function processMail() {
     gameData.mail += gameData.mailPerClick
     document.getElementById("mailProcessed").innerHTML = gameData.mail + " Mails processed"
     totalMailCount()
 }
 
+// this function counts the total number of mails processed.
 function totalMailCount() {
   gameData.totalMail += gameData.mailPerClick
   document.getElementById("totalMailProcessed").innerHTML = gameData.totalMail + " Total mails processed"
 }
 
+//lets you buy upgrades that double your mailprocessing power (payed my mails)
 function buyMailPerClick() {
 	if (gameData.mail >= gameData.mailPerClickCost){
 		gameData.mail -= gameData.mailPerClickCost
 		gameData.mailPerClick += 1
 		gameData.mailPerClickCost *= 2
 		document.getElementById("mailProcessed").innerHTML = gameData.mail + " Mail Processed"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade manual mail processing (Currently Level" + gameData.mailPerClick + ") Cost: " + gameData.mailPerClickCost + " Mails"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade manual mail processing (Currently Level " + gameData.mailPerClick + ") Cost: " + gameData.mailPerClickCost + " Mails"
 	}
 }
 
@@ -32,9 +44,6 @@ function buyMailPerClick() {
 
   var saveGameLoop = window.setInterval(function() {
 	localStorage.setItem("IncremailsSave", JSON.stringify(gameData))
-  }, 15000)
+  }, 1000)
 
-  var savegame = JSON.parse(localStorage.getItem("IncremailsSave"))
-if (savegame !== null) {
-  gameData = savegame
-}
+  
